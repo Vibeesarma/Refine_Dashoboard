48a16ccfaf5c7c961ad0b3a4d4b272b807014385
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-02-26 02:53:03 +0000

    create properties finished

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -12,23 +12,24 @@ const CustomButton = ({
   handleClick,
 }: CustomButtonProps) => {
   return (
-    <Button sx={{
-      flex:fullWidth?1:'unset',
-      padding:'10px 15px',
-      width:fullWidth?'100%':'fit-content',
-      minWidth:130,
-      backgroundColor,
-      color,
-      fontWeight:600,
-      gap:'10px',
-      textTransform:'capitalize',
-      '&:hover':{
-        opacity:0.9,
-        backgroundColor
-      }
-    }}
-    onClick={handleClick}
-    
+    <Button
+      type={type === "submit" ? "submit" : "button"}
+      sx={{
+        flex: fullWidth ? 1 : "unset",
+        padding: "10px 15px",
+        width: fullWidth ? "100%" : "fit-content",
+        minWidth: 130,
+        backgroundColor,
+        color,
+        fontWeight: 600,
+        gap: "10px",
+        textTransform: "capitalize",
+        "&:hover": {
+          opacity: 0.9,
+          backgroundColor,
+        },
+      }}
+      onClick={handleClick}
     >
       {icon}
       {title}
